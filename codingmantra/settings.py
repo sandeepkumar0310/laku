ba963ba3ae369567af8137d3abf6833ee1399a8c
luckylaku <luckydeveloper123@gmail.com>
2024-09-21 14:38:49 +0530

    Your commit message for the submodule

--- codingmantra/settings.py
+++ codingmantra/settings.py
@@ -75,20 +75,14 @@ WSGI_APPLICATION = 'codingmantra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
